86ee3f0e752b24ba32e7b2cbae26a78b155a3f38
Giorgos Kormpakis <korbakis@synelixis.com>
2020-09-18 15:05:28 +0300

    First commit.

---
+++ articles/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article
+
+
+# Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article': article})
